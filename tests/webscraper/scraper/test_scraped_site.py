00c0f3b76ea1b7fd9cbaa768b9ac8caa0abf2331
WebLudus <webluduspl+github@gmail.com>
2022-07-14 21:40:53 +0200

    some cleaning

--- tests/webscraper/scraper/test_scraped_site.py
+++
@@ -1,5 +0,0 @@
-from webscraper.scraper.scraped_site import ImageSource
-
-
-class TestImageSource:
-    ...
--- tests/webscraper/scraper/test_scraper.py
+++
@@ -1,6 +0,0 @@
-from webscraper.scraper.scraper import (
-    get_html_dom,
-    find_all_images,
-    find_next_page,
-    get_request,
-)
